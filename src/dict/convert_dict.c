d63f74d097df3cbfa169f55928e74e20ea0f7192
paul <paul.chazalmartin@protonmail.com>
2026-01-26 17:42:08 +0100

    cc fix convert dict

--- src/dict/convert_dict.c
+++ src/dict/convert_dict.c
@@ -6,7 +6,7 @@
 /*   By: miniplop <miniplop@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 14:19:38 by miniplop          #+#    #+#             */
-/*   Updated: 2026/01/25 11:28:00 by miniplop         ###   ########.fr       */
+/*   Updated: 2026/01/26 17:41:46 by miniplop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,7 @@
 
 static char	*build_exp_line(char *key, char *value)
 {
-	char	*tmp;
 	char	*line;
-	char	*tmp2;
 
 	line = ft_calloc(sizeof(char), ft_strlen(key) + ft_strlen(value) + 4);
 	if (line)
@@ -99,7 +97,7 @@ char	**dict_to_env(t_dict *dict, int flag)
 	env = ft_calloc(sizeof(char *), (count + 1));
 	if (!env)
 		return (NULL);
-	if (fill_env(dict, env, ENV) < 0)
+	if (fill_env(dict, env, flag) < 0)
 		return (NULL);
 	return (env);
 }
