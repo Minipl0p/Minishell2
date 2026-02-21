6cdb8c00d6fcafd5cdee2e80f0b469f7ce507549
Minipl0p <paul.chazalmartin@protonmail.com>
2026-02-21 17:21:35 +0100

    ++++

--- src/expand/expand_interpretor.c
+++ src/expand/expand_interpretor.c
@@ -6,7 +6,7 @@
 /*   By: miniplop <miniplop@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 08:48:36 by miniplop          #+#    #+#             */
-/*   Updated: 2026/02/21 15:34:34 by pchazalm         ###   ########.fr       */
+/*   Updated: 2026/02/21 17:09:50 by pchazalm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,23 +72,25 @@ char	*expand_str_vars(char *old_str, t_dict *d_env)
 	int		flag;
 	char	*str;
 
-	i = 0;
+	i = -1;
 	flag = 0;
 	if (!old_str)
 		return (NULL);
 	str = ft_strdup(old_str);
 	if (!str)
 		return (NULL);
-	while (*str && str[i])
+	while (*str && str[++i])
 	{
 		if (flag == 0 && (str[i] == '\'' || str[i] == '\"'))
 			flag += (str[i] == '\'') + 2 * (str[i] == '\"');
-		else if (flag != 1 && str[i] == '$')
+		else if (flag != 1 && str[i] == '$' && str[i + 1] && str[i + 1] != '$' && str[i + 1] != '\'' && str[i + 1] != '"')
+		{
 			str = expand_key(str, i + 1, d_env);
+			i = -1;
+			continue ;
+		}
 		else if ((flag == 1 && str[i] == '\'') || (flag == 2 && str[i] == '\"'))
 			flag -= (str[i] == '\'') + 2 * (str[i] == '\"');
-		else
-			i++;
 		if (!str[i])
 			break ;
 		if (!str)
