530961fa2a33ff2d53893aba731d78f3e8df4556
Ix-Tap3 <patxicaplat.pro@gmail.com>
2026-03-03 10:28:05 +0100

    make main.c complient with 42 norm

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: miniplop <miniplop@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/19 11:05:04 by miniplop          #+#    #+#             */
-/*   Updated: 2026/02/21 16:47:21 by pchazalm         ###   ########.fr       */
+/*   Updated: 2026/03/03 10:27:11 by pcaplat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,30 @@
 #include "libft/Includes/ft_string.h"
 #include <fcntl.h>
 #include <readline/readline.h>
-#include <stdio.h>
 
-static t_dict *init(int ac, char **av, char **env) {
-  t_dict *d_env;
-  struct sigaction sa;
+static t_dict	*init(int ac, char **av, char **env)
+{
+	t_dict				*d_env;
+	struct sigaction	sa;
 
-  (void)ac;
-  (void)av;
-  init_signal(&sa, NULL, MAIN);
-  signal(SIGQUIT, SIG_IGN);
-  // print_banner();
-  d_env = init_d_env(env);
-  return (d_env);
+	(void)ac;
+	(void)av;
+	init_signal(&sa, NULL, MAIN);
+	signal(SIGQUIT, SIG_IGN);
+	print_banner();
+	d_env = init_d_env(env);
+	return (d_env);
 }
 
-int main(int ac, char **av, char **env) {
-  t_dict *d_env;
+int	main(int ac, char **av, char **env)
+{
+	t_dict	*d_env;
 
-  d_env = init(ac, av, env);
-  if (!d_env)
-    return (1);
-  process(d_env);
-  dict_destroy(d_env, free);
-  rl_clear_history();
-  return (0);
+	d_env = init(ac, av, env);
+	if (!d_env)
+		return (1);
+	process(d_env);
+	dict_destroy(d_env, free);
+	rl_clear_history();
+	return (0);
 }
